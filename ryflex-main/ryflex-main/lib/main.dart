1d9d3bee4e65e680e7a515cdd194feefb22b9602
irfanBaloch <irfanalibaloch4611@gmail.com>
2023-10-09 09:42:22 +0500

    flutter project 20sw073 and19-20sw065

---
+++ ryflex-main/ryflex-main/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:ryflex/map_page.dart';
+import 'package:ryflex/splash_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp()
+      .then((value) => print("Firebase Initialization completed..."));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashPage(),
+    );
+  }
+}
